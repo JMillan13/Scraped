f041d223f9ce5abe2f5a37d4555fa443d58fc4e5
Joseph Millan <https://github.com/JMillan13>
2019-07-25 20:06:49 -0400

    PORT for env

--- server.js
+++ server.js
@@ -3,6 +3,30 @@ var exphbs  = require('express-handlebars');
 var logger = require("morgan");
 var mongoose = require("mongoose");
 var path = require("path");
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+var PORT = process.env.PORT || 3000;
+
+
+
+
+
+// Initialize Express
+var app = express();
+
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
 
 
@@ -17,32 +41,6 @@ mongoose.connect(MONGODB_URI, mongoose_options, function (err) {
     console.log("Succeeded connected to: " + MONGODB_URI);
   }
 });
-
-// Our scraping tools
-// Axios is a promised-based http library, similar to jQuery's Ajax method
-// It works on the client and on the server
-var axios = require("axios");
-var cheerio = require("cheerio");
-
-// Require all models
-var db = require("./models");
-
-var PORT = process.env.PORT || 3000;
-
-// Initialize Express
-var app = express();
-
-
-// Configure middleware
-
-// Use morgan logger for logging requests
-app.use(logger("dev"));
-// Parse request body as JSON
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Make public a static folder
-app.use(express.static("public"));
-
 
 // Routes
 var url = "https://www.espn.com/nba/"
